93c540eace2df1e7323a206207f9333e512b29bc
Horacio <hgx95@hotmail.com>
2015-09-27 12:54:25 -0500

    Version 1.0 Beta

--- pongGame/gameWindow.cs
+++ pongGame/gameWindow.cs
@@ -10,16 +10,18 @@ using System.Windows.Forms;
 
 namespace pongGame
 {
-        
+    
 
 
     public partial class gameWindow : Form
     {
 
-        
-        protected int yPlayer1 = 132;
-        protected int yPlayer2 = 132;
-        
+        protected int x = 10, y= 10;
+        protected int yPlayer1 = 132, player1Score = 0;
+        protected int yPlayer2 = 132, player2Score = 0;
+        protected int xBall = 380;
+        protected int yBall = 135;
+
 
         public gameWindow()
         {
@@ -50,7 +52,56 @@ namespace pongGame
         private void timerForGame_Tick(object sender, EventArgs e)
         {
 
+
+            moveBall();
             
+            
+            ball.Location = new Point(xBall, yBall);
+            checkIfBounce();
+            checkIfLoose();
+
+            
+        }
+
+        private void moveBall()
+        {
+            
+            xBall +=  x;
+            yBall += y;
+        }
+
+        private void checkIfBounce()
+        {
+            if (xBall >= 730 &&  yBall <= (yPlayer2+80) && yBall >= yPlayer2)
+            {
+                x = x * (-1);
+            }
+
+            if (xBall <= 42 && yBall <= (yPlayer1 + 80) && yBall >= yPlayer1)
+            {
+                x = x * (-1);
+            }
+
+
+            if (yBall >= 340 || yBall <= 10)
+            {
+                y = y * (-1);
+            }
+
+            
+
+        }
+
+        private void checkIfLoose()
+        {
+            if (xBall >= 744)
+            {
+                player1Score += 1;
+
+                label1.Text = player1Score.ToString();
+
+                xBall = 380;
+            }
         }
 
         private void gameWindow_KeyPress(object sender, KeyPressEventArgs e)
@@ -96,20 +147,20 @@ namespace pongGame
 
                 case Keys.W:
 
-                    if (yPlayer2 > 20)
-                    {
-                        yPlayer2 -= 20;
-                        playerTwo.Location = new Point(734, yPlayer2);
-                    }
+                        if (yPlayer2 > 20)
+                        {
+                            yPlayer2 -= 20;
+                            playerTwo.Location = new Point(734, yPlayer2);
+                        }
                     break;
 
                 case Keys.S:
 
-                    if (yPlayer2 < 260)
-                    {
-                        yPlayer2 += 20;
-                        playerTwo.Location = new Point(734, yPlayer2);
-                    }
+                        if (yPlayer2 < 260)
+                        {
+                            yPlayer2 += 20;
+                            playerTwo.Location = new Point(734, yPlayer2);
+                        }
 
                     break;
 
